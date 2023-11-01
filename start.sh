fdc4b6af349b998fb1eb9e135423d1c912609261
aida0710 <51500566+aida0710@users.noreply.github.com>
2023-11-02 01:07:32 +0900

    ubuntu用の自動起動シェルを追加

---
+++ start.sh
@@ -0,0 +1,19 @@
+#現在実行しているnext.jsを削除
+rm -r -f ./profile
+echo "情報 - 現在実行しているnext.jsを削除しました"
+
+#gitからProfileをクローン
+echo "情報 - aida0710/profileリポジトリをクローンします"
+git clone git@github.com:aida0710/profile.git
+echo "完了 - gitから必要なリポジトリをクローンしました"
+
+# ビルド&起動処理
+cd /home/aida/profile
+npm install
+npm run build
+screen -S profile -X quit
+screen -dmS profile
+sleep 1s
+screen -S profile -X stuff 'npm start'`echo -ne '\015'`
+
+echo "完了 - 起動処理が完了しました"
