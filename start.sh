5fe51b8b2b06ee682db0ca513f11381ad76637de
相田 優希 <51500566+aida0710@users.noreply.github.com>
2023-11-02 03:33:30 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "情報 - 現在実行しているnext.jsを削除しました"
 
 #gitからProfileをクローン
 echo "情報 - aida0710/profileリポジトリをクローンします"
-git clone git@github.com:aida0710/profile.git
+git clone https://github.com/aida0710/profile.git
 echo "完了 - gitから必要なリポジトリをクローンしました"
 
 # ビルド&起動処理
