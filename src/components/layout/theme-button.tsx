7b97d3d77d918f63f92012f99e885ed3ef50741c
aida0710 <51500566+aida0710@users.noreply.github.com>
2023-11-04 06:07:49 +0900

    tooltipを削除

--- src/components/layout/theme-button.tsx
+++ src/components/layout/theme-button.tsx
@@ -1,47 +1,37 @@
 "use client";
-import { useTheme } from "next-themes";
-import React, { useEffect, useState } from "react";
-import { Button } from "@nextui-org/react";
-import { UseThemeProps } from "next-themes/dist/types";
-import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
-import { Tooltip } from "@nextui-org/tooltip";
+import {Button} from "@nextui-org/react";
+import {useTheme} from "next-themes";
+import {UseThemeProps} from "next-themes/dist/types";
+import React, {useEffect, useState} from "react";
+import {BsFillMoonStarsFill, BsFillSunFill} from "react-icons/bs";
 
 export function ThemeButton() {
-  const [mounted, setMounted] = useState(false);
-  const { setTheme, theme }: UseThemeProps = useTheme();
+    const [mounted, setMounted] = useState(false);
+    const {setTheme, theme}: UseThemeProps = useTheme();
 
-  const handleSetTheme = (): void => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
+    const handleSetTheme = (): void => {
+        setTheme(theme === "light" ? "dark" : "light");
+    };
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
 
-  if (!mounted) return null;
+    if (!mounted) return null;
 
-  return (
-    <Tooltip
-      content={
-        <div>
-          <div className="flex justify-center">Theme Change</div>
-          <div className="flex justify-center">light / dark</div>
-        </div>
-      }
-    >
-      <Button
-        className="block p-2"
-        radius="full"
-        variant="ghost"
-        onClick={handleSetTheme}
-        isIconOnly
-      >
-        {theme === "light" ? (
-          <BsFillMoonStarsFill className="w-full h-full" />
-        ) : (
-          <BsFillSunFill className="w-full h-full" />
-        )}
-      </Button>
-    </Tooltip>
-  );
+    return (
+        <Button
+            className="block p-2"
+            radius="full"
+            variant="ghost"
+            onClick={handleSetTheme}
+            isIconOnly
+        >
+            {theme === "light" ? (
+                <BsFillMoonStarsFill className="w-full h-full"/>
+            ) : (
+                <BsFillSunFill className="w-full h-full"/>
+            )}
+        </Button>
+    );
 }
