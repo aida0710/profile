fc7a191d3b4f49aa93ff5811086c3cf348da2042
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-22 00:28:47 +0900

    コード整形

--- src/app/awards/data-store.tsx
+++ src/app/awards/data-store.tsx
@@ -29,7 +29,7 @@ export const Awards: AwardsProps[] = [
         date: '2023年09月22日',
     },
     {
-        organization: "Transeeds Inc.",
+        organization: 'Transeeds Inc.',
         description: 'JAPAN WEB3.0 AWARD αU賞 受賞',
         image: 'japan-web3-award.png',
         link: 'https://prtimes.jp/main/html/rd/p/000000024.000056442.html',
