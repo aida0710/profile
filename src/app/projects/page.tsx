3d3a3b683db72674fe460cd6a3d102327b4f565e
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-21 23:44:15 +0900

    toolセットを追加

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,56 +4,11 @@ import {Skills} from '@/components/skills';
 import {Card, CardBody} from '@nextui-org/card';
 import Image from 'next/image';
 import Link from 'next/link';
+import {projects, ProjectsProps} from '@/app/projects/data-store';
 
 export const metadata: Metadata = {
     title: 'Projects',
 };
-
-interface ProjectsProps {
-    title: string;
-    description: string;
-    image: string;
-    link: string;
-    language: string;
-}
-
-const projects: ProjectsProps[] = [
-    {
-        title: 'Catsial',
-        description: 'Web単語帳アプリ',
-        image: 'catsial.png',
-        link: 'https://www.catsial.app',
-        language: 'TypeScript',
-    },
-    {
-        title: 'Zircon Lang',
-        description: 'Rust製自作言語 インタープリタ式 開発中',
-        image: 'zircon-lang.png',
-        link: 'https://github.com/aida0710/zircon-lang',
-        language: 'Rust',
-    },
-    {
-        title: 'Blur Focus',
-        description: '初めて作成したクローム拡張機能。文章を読むときに文字が滑るので、文字が滑らないように対策する拡張機能',
-        image: 'blur-focus.png',
-        link: 'https://github.com/aida0710/blur-focus',
-        language: 'TypeScript',
-    },
-    {
-        title: 'Folivora',
-        description: 'マインクラフトサーバのプラグイン。真面目に設計を頑張ったプロジェクト',
-        image: 'folivora.png',
-        link: 'https://github.com/aida0710/Folivora',
-        language: 'PHP',
-    },
-    {
-        title: 'Profile',
-        description: 'この自己紹介サイトのソースコード',
-        image: 'profile.png',
-        link: 'https://github.com/aida0710/profile',
-        language: 'TypeScript',
-    },
-];
 
 export default async function Page() {
     return (
