9e743c54bb3ebc50af29031be692d0836ef649ff
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-03-23 18:58:22 +0900

    プロジェクトを更新

--- src/app/projects/data-store.tsx
+++ src/app/projects/data-store.tsx
@@ -1,6 +1,6 @@
 export interface ProjectsProps {
     title: string;
-    description: string;
+    description: string[];
     image: string;
     link: string;
     language: string;
@@ -9,35 +9,42 @@ export interface ProjectsProps {
 export const projects: ProjectsProps[] = [
     {
         title: 'Catsial',
-        description: 'Web単語帳アプリ',
+        description: ['Web単語帳アプリ', '現在開発中 データベースはMySQLを使用'],
         image: 'catsial.png',
         link: 'https://www.catsial.app',
         language: 'TypeScript',
     },
     {
+        title: 'Web ネットワーク シミュレータ',
+        description: ['100pro 5期で作成したネットワークシミュレータ', 'フロントエンドなどを担当し、Next.jsで開発', '公開サイト https://nw-sim.net/'],
+        image: 'network-simulator.png',
+        link: 'https://github.com/orgs/web-network-simulator/repositories',
+        language: 'TypeScript / Python',
+    },
+    {
         title: 'Zircon Lang',
-        description: 'Rust製自作言語 インタープリタ式 開発中',
+        description: ['Kotlin製自作インタープリター言語', '開発中(字句解析器/構文解析器/インタープリター実装済み)'],
         image: 'zircon-lang.png',
         link: 'https://github.com/aida0710/zircon-lang',
-        language: 'Rust',
+        language: 'Kotlin',
     },
     {
         title: 'Blur Focus',
-        description: '初めて作成したクローム拡張機能。文章を読むときに文字が滑るので、文字が滑らないように対策する拡張機能',
+        description: ['初めて作成したクローム拡張機能。', '文章を読むときに文字が滑るので、文字が滑らないように対策する拡張機能'],
         image: 'blur-focus.png',
         link: 'https://github.com/aida0710/blur-focus',
         language: 'TypeScript',
     },
     {
         title: 'Folivora',
-        description: 'マインクラフトサーバのプラグイン。真面目に設計を頑張ったプロジェクト',
+        description: ['マインクラフトサーバのプラグイン。真面目に設計を頑張ったプロジェクト'],
         image: 'folivora.png',
         link: 'https://github.com/aida0710/Folivora',
         language: 'PHP',
     },
     {
         title: 'Profile',
-        description: 'この自己紹介サイトのソースコード',
+        description: ['この自己紹介サイトのソースコード'],
         image: 'profile.png',
         link: 'https://github.com/aida0710/profile',
         language: 'TypeScript',
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -27,7 +27,11 @@ export default async function Page() {
                         <CardBody>
                             <h2 className='text-sm'>言語：{project.language}</h2>
                             <h3 className='my-2 font-semibold'>{project.title}</h3>
-                            <p className='mb-1'>{project.description}</p>
+                            <p className='mb-1'>{project.description.map((item) => (
+                                <div>
+                                    <p className='text-md'>{item}</p>
+                                </div>
+                            ))}</p>
                             <Image
                                 src={`/images/projects/${project.image}`}
                                 alt={project.title}
