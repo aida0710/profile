c8a249930f9e3be3bbae20648b867ffb03c93d01
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-06-30 10:05:45 +0900

    プロジェクトをクリックしたときにモーダルが表示されるように

--- src/app/projects/data-store.tsx
+++ src/app/projects/data-store.tsx
@@ -2,7 +2,12 @@ export interface ProjectsProps {
     title: string;
     description: string[];
     image: string;
-    link: string;
+    links: {
+        [key: string]: {
+            description: string;
+            url: string;
+        };
+    };
     language: string;
 }
 
@@ -11,49 +16,96 @@ export const projects: ProjectsProps[] = [
         title: 'Catsial',
         description: ['Web単語帳アプリ', '現在開発中 データベースはMySQLを使用'],
         image: 'catsial.png',
-        link: 'https://www.catsial.app',
+        links: {
+            web: {
+                description: '公開サイト(現在公開停止中)',
+                url: 'https://www.catsial.app',
+            },
+            awards: {
+                description: '岐阜協立大学 第6回 高校生ビジネスアイデアコンテスト',
+                url: 'https://www.gku.ac.jp/topics/event/post-354.html',
+            }
+        },
         language: 'TypeScript',
     },
     {
         title: 'Web Network Simulator',
-        description: ['100pro 5期で作成したネットワークシミュレータ', 'フロントエンドなどを担当し、Next.jsで開発', '公開サイト https://nw-sim.net/'],
+        description: ['100pro 5期で作成したネットワークシミュレータ', '主にフロントエンドなどを担当し、Next.jsで開発'],
         image: 'network-simulator.png',
-        link: 'https://github.com/orgs/web-network-simulator/repositories',
+        links: {
+            web: {
+                description: '公開サイト',
+                url: 'https://www.nw-sim.net/',
+            },
+            github: {
+                description: 'Github Repositories',
+                url: 'https://github.com/orgs/web-network-simulator/repositories',
+            },
+        },
         language: 'TypeScript / Python',
     },
     {
         title: 'Desktop Mascot',
         description: ['100pro 5期で作成したデスクトップマスコット', 'Unity + C# + Google Calendar Api for .Netで開発'],
         image: 'desktop-mascot.png',
-        link: 'https://100program.jp/',
+        links: {
+            web: {
+                description: '100Program公式サイト',
+                url: 'https://100program.jp/',
+            },
+        },
         language: 'C#',
     },
     {
         title: 'Zircon Lang',
         description: ['Kotlin製自作インタープリター言語', '開発中(字句解析器/構文解析器/インタープリター実装済み)'],
         image: 'zircon-lang.png',
-        link: 'https://github.com/aida0710/zircon-lang',
+        links: {
+            github: {
+                description: 'Github Repository',
+                url: 'https://github.com/aida0710/zircon-lang',
+            },
+        },
         language: 'Kotlin',
     },
     {
         title: 'Blur Focus',
         description: ['初めて作成したクローム拡張機能。', '文章を読むときに文字が滑るので、文字が滑らないように対策する拡張機能'],
         image: 'blur-focus.png',
-        link: 'https://github.com/aida0710/blur-focus',
+        links: {
+            github: {
+                description: 'Github Repository',
+                url: 'https://github.com/aida0710/blur-focus',
+            },
+        },
         language: 'TypeScript',
     },
     {
         title: 'Folivora',
         description: ['マインクラフトサーバのプラグイン。真面目に設計を頑張ったプロジェクト'],
         image: 'folivora.png',
-        link: 'https://github.com/aida0710/Folivora',
+        links: {
+            github: {
+                description: 'Github Repository',
+                url: 'https://github.com/aida0710/Folivora',
+            },
+        },
         language: 'PHP',
     },
     {
         title: 'Profile',
         description: ['この自己紹介サイトのソースコード'],
         image: 'profile.png',
-        link: 'https://github.com/aida0710/profile',
+        links: {
+            web: {
+                description: '公開サイト',
+                url: 'https://www.aida0710.work/',
+            },
+            github: {
+                description: 'Github Repository',
+                url: 'https://github.com/aida0710/profile',
+            },
+        },
         language: 'TypeScript',
     },
 ];
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,49 +1,23 @@
 import React from 'react';
 import {Metadata} from 'next';
 import {Skills} from '@/components/skills';
-import {Card, CardBody} from '@nextui-org/card';
-import Image from 'next/image';
-import Link from 'next/link';
 import {projects, ProjectsProps} from '@/app/projects/data-store';
+import ProjectCardModal from '@/app/projects/components/project-card-modal';
 
 export const metadata: Metadata = {
     title: 'Projects',
 };
 
-export default async function Page() {
+export default function Page() {
     return (
         <Skills
             title='Projects'
-            description='クリックしたらリポジトリやサイトに飛べます！'>
+            description='自分が開発した又は携わったプロジェクト'>
             {projects.map((project: ProjectsProps, index: number) => (
-                <Link
-                    target='_blank'
-                    href={project.link}
-                    key={index}>
-                    <Card
-                        className='h-full'
-                        isHoverable
-                        isPressable>
-                        <CardBody>
-                            <h2 className='text-sm'>言語：{project.language}</h2>
-                            <h3 className='my-2 font-semibold'>{project.title}</h3>
-                            <p className='mb-1'>
-                                {project.description.map((item: string, index: number) => (
-                                    <div key={index}>
-                                        <p className='text-md'>{item}</p>
-                                    </div>
-                                ))}
-                            </p>
-                            <Image
-                                src={`/images/projects/${project.image}`}
-                                alt={project.title}
-                                className='mt-auto aspect-video rounded-lg'
-                                width={1920}
-                                height={1080}
-                            />
-                        </CardBody>
-                    </Card>
-                </Link>
+                <ProjectCardModal
+                    key={index}
+                    {...project}
+                />
             ))}
         </Skills>
     );
