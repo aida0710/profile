8426f496592d1c40f8d87de9fe3e11d23c46f6c1
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-08-13 11:56:41 +0900

    style記述を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import {Image} from '@nextui-org/image';
 import {SnsIcons} from '@/app/index/sns-icons';
 import {StatusMessage} from '@/app/index/status-message';
 import {NoSSR} from '@/app/index/utils/no-ssr';
-import Link from 'next/link';
-import {Button} from '@nextui-org/react';
-import {AppWindowIcon, AwardIcon, PenToolIcon} from 'lucide-react';
+import {MailIcon} from 'lucide-react';
 import {ChronologicalTable} from '@/app/index/chronological-table';
+import {Chip} from '@nextui-org/chip';
+import {SelectionButton} from '@/app/index/SelectionButton';
 
 export default async function Page() {
     return (
@@ -23,8 +23,10 @@ export default async function Page() {
                 />
             </div>
             <h6 className='text-lg font-medium tracking-wide md:text-2xl'>Masaki Aida / 相田 優希</h6>
-            <p className='text-md mt-2 font-normal tracking-wide md:text-xl'>19歳 千葉県船橋市在住</p>
-            <p className='text-md font-normal tracking-wide md:text-xl'>Frontend/Backend Engineer</p>
+            <div className='text-md my-3 font-normal tracking-wide md:text-xl'>
+                <p>19歳 千葉県船橋市在住</p>
+                <p>Frontend/Backend Engineer</p>
+            </div>
             <SnsIcons />
             <NoSSR>
                 <StatusMessage />
